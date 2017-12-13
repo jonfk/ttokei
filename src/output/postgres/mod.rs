02c88385376f5fd4cf3ed40a7218687e5b79ae32
Jonathan Fok kan <jfokkan@gmail.com>
2017-12-12 23:10:45 -0500

    Added smaller chunks for saving language stats

--- src/output/postgres/mod.rs
+++ src/output/postgres/mod.rs
@@ -77,7 +77,7 @@ impl PgOutputter {
 
 
         if bulk_stats_values.len() > 65534 {
-            for stats_values in bulk_stats_values.chunks(65534) {
+            for stats_values in bulk_stats_values.chunks(10000) {
                 let language_stats_id = insert::create_language_stats(&self.conn,
                                                                       stats_values.to_vec());
                 debug!("inserted language stats {}", language_stats_id);
