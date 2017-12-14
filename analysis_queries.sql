290c138b84dd79131b1bc61a2fc2871880b645f5
Jonathan Fok kan <jonathan.fokkan@appdirect.com>
2017-12-14 17:55:19 -0500

    Fixed language growth query and added git_tags

--- analysis_queries.sql
+++ analysis_queries.sql
@@ -3,6 +3,9 @@
 -- Meta Information --
 ----------------------
 
+-- number of possible data points in time
+select count(*) from git_tags;
+
 -- number of parses (data points in time)
 select count(*) from parses;
 
@@ -26,7 +29,7 @@ select (select count(*) from completed_parses)::float / (select count(*) from gi
 
 -- language (lines, code, comments, files) over time
 -- (selects distinct days and loses languages on same day but different timestamps)
-select distinct cast (p.time as date), p.git_tag, sum(l.lines), sum(l.code), sum(l.comments)
+select distinct cast (p.time as date), p.time, p.git_tag, sum(l.lines) as total_lines, sum(l.code) as code, sum(l.comments) as comments
 from parses p
 inner join languages l on l.parse_id = p.parse_id
 group by p.parse_id
