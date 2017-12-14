8c9da825654f1fbb858b5ea5b1b37539662c9d51
Jonathan Fok kan <jonathan.fokkan@appdirect.com>
2017-12-14 11:22:11 -0500

    Added more analysis queries

--- analysis_queries.sql
+++ analysis_queries.sql
@@ -1,6 +1,15 @@
 
+----------------------
+-- Meta Information --
+----------------------
+
 -- number of parses (data points in time)
 select count(*) from parses;
+
+-- Time taken to parse codebase over time (assuming a continuous parsing without interruptions)
+select (select min(created_on) from completed_parses) as first_parse,
+(select max(created_on) from completed_parses) as last_parse,
+age((select max(created_on) from completed_parses), (select min(created_on) from completed_parses)) as time_taken;
 
 -- span of time of information
 select (select min(time) from parses) as first_timepoint,
@@ -9,6 +18,11 @@ age((select max(time) from parses), (select min(time) from parses)) as span_of_t
 
 -- percentage parses completion
 select (select count(*) from completed_parses)::float / (select count(*) from git_tags) * 100;
+
+
+--------------
+-- Analysis --
+--------------
 
 -- language (lines, code, comments, files) over time
 -- (selects distinct days and loses languages on same day but different timestamps)
